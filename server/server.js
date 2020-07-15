a5033a92fad31baf9f1af675fb62149e6cac50da
RotonEvan <debajyotih@iitbhilai.ac.in>
2020-07-15 23:48:01 +0530

    server.js integrates all files

--- server/server.js
+++ server/server.js
@@ -29,6 +29,78 @@ const handleRequest = function (request, response) {
   } else if (request.url === '/style.css') {
     response.writeHead(200, { 'Content-Type': 'text/css' });
     response.end(fs.readFileSync('client/style.css'));
+  } else if (request.url === '/css/style.css') {
+    response.writeHead(200, { 'Content-Type': 'text/css' });
+    response.end(fs.readFileSync('client/css/style.css'));
+  } else if (request.url === '/css/bootstrap.min.css') {
+    response.writeHead(200, { 'Content-Type': 'text/css' });
+    response.end(fs.readFileSync('client/css/bootstrap.min.css'));
+  } else if (request.url === '/css/animsition.min.css') {
+    response.writeHead(200, { 'Content-Type': 'text/css' });
+    response.end(fs.readFileSync('client/css/animsition.min.css'));
+  } else if (request.url === '/favicon/apple-touch-icon.png') {
+    response.writeHead(200, { 'Content-Type': 'image/png' });
+    response.end(fs.readFileSync('client/favicon/apple-touch-icon.png'));
+  } else if (request.url === '/favicon/favicon-16x16.png') {
+    response.writeHead(200, { 'Content-Type': 'image/png' });
+    response.end(fs.readFileSync('client/favicon/favicon-16x16.png'));
+  } else if (request.url === '/favicon/favicon-32x32.png') {
+    response.writeHead(200, { 'Content-Type': 'image/png' });
+    response.end(fs.readFileSync('client/favicon/favicon-32x32.png'));
+  } else if (request.url === '/favicon/site.html') {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(fs.readFileSync('client/favicon/site.html'));
+  } else if (request.url === '/fonts/butler_extrabold-webfont.eot') {
+    response.writeHead(200, { 'Content-Type': 'font/eot' });
+    response.end(fs.readFileSync('client/fonts/butler_extrabold-webfont.eot'));
+  } else if (request.url === '/fonts/butler_extrabold-webfont.html') {
+    response.writeHead(200, { 'Content-Type': 'font/html' });
+    response.end(fs.readFileSync('client/fonts/butler_extrabold-webfont.html'));
+  } else if (request.url === '/fonts/butler_extrabold-webfont.ttf') {
+    response.writeHead(200, { 'Content-Type': 'font/ttf' });
+    response.end(fs.readFileSync('client/fonts/butler_extrabold-webfont.ttf'));
+  } else if (request.url === '/fonts/butler_extrabold-webfont.woff') {
+    response.writeHead(200, { 'Content-Type': 'font/woff' });
+    response.end(fs.readFileSync('client/fonts/butler_extrabold-webfont.woff'));
+  } else if (request.url === '/fonts/butler_extrabold-webfont41d.eot') {
+    response.writeHead(200, { 'Content-Type': 'font/eot' });
+    response.end(fs.readFileSync('client/fonts/butler_extrabold-webfont41d.eot'));
+  } else if (request.url === '/img/bg-body.png') {
+    response.writeHead(200, { 'Content-Type': 'image/png' });
+    response.end(fs.readFileSync('client/img/bg-body.png'));
+  } else if (request.url === '/img/bg-promo.png') {
+    response.writeHead(200, { 'Content-Type': 'image/png' });
+    response.end(fs.readFileSync('client/img/bg-promo.png'));
+  } else if (request.url === '/js/animation.gsap.min.js') {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.end(fs.readFileSync('client/js/animation.gsap.min.js'));
+  } else if (request.url === '/js/animation.min.js') {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.end(fs.readFileSync('client/js/animation.min.js'));
+  } else if (request.url === '/js/bootstrap.min.js') {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.end(fs.readFileSync('client/js/bootstrap.min.js'));
+  } else if (request.url === '/js/jquery.min.js') {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.end(fs.readFileSync('client/js/jquery.min.js'));
+  } else if (request.url === '/js/popper.min.js') {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.end(fs.readFileSync('client/js/popper.min.js'));
+  } else if (request.url === '/js/script.js') {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.end(fs.readFileSync('client/js/script.js'));
+  } else if (request.url === '/js/ScrollMagic.min.js') {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.end(fs.readFileSync('client/js/ScrollMagic.min.js'));
+  } else if (request.url === '/js/smoothscroll.js') {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.end(fs.readFileSync('client/js/smoothscroll.js'));
+  } else if (request.url === '/js/TweenMax.min.js') {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.end(fs.readFileSync('client/js/TweenMax.min.js'));
+  } else if (request.url === '/room.html') {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(fs.readFileSync('client/room.html'));
   } else {
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.end(fs.readFileSync('client/index.html'));

29d710e33e70fe77bd552787474cbb917d62fec2
RotonEvan <debajyotih@iitbhilai.ac.in>
2020-07-30 02:06:10 +0530

    if failed or closed delete pc

--- client/webrtc.js
+++ client/webrtc.js
@@ -178,7 +178,7 @@ var count = 0;
 function checkPeerDisconnect(event, peerUuid) {
   var state = peerConnections[peerUuid].pc.iceConnectionState;
   console.log(`connection with peer ${peerUuid} ${state}`);
-  if (state === "failed" || state === "closed" || state === "disconnected") {
+  if (state === "failed" || state === "closed") {
     delete peerConnections[peerUuid];
     document.getElementById('videos').removeChild(document.getElementById('remoteVideo_' + peerUuid));
     updateLayout();
